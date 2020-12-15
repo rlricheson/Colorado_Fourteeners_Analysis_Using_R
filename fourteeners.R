b45146e58b21dc0e969b87e2b94b2b371631b65c
Randall Richeson <randy.richeson@gmail.com>
2020-12-14 22:43:41 -0700

    Update fourteeners.R

--- fourteeners.R
+++ fourteeners.R
@@ -1,4 +1,20 @@
 
+id = c(1,2,3,4,5) 
+id
+id <- c(1,2,3,4,5)  
+c(1,2,3,4,5) -> id
+id  
+name = c("Mt. Elbert","Mt. Massive","Mt. Harvard","Blanca Peak","La Plata Peak")
+feet = c(14433,14421,14420,14345,14336)
+ascents = c(12611,8915,6859,4525,8485)
+CO14ers = data.frame(id=id,name=name,feet=feet,ascents=ascents)
+CO14ers
+12*(CO14ers$feet)
+mean(CO14ers$feet)
+plot(CO14ers$feet, type="l")
+plot(CO14ers$feet, type="h")
+plot(CO14ers$feet, type="b")
+
 #-- Import the dataset from a comma separated value file
 fourteeners <- read.csv("C:/job_search/appian/fourteeners.csv")
 
@@ -6,13 +22,24 @@ fourteeners <- read.csv("C:/job_search/appian/fourteeners.csv")
 View(fourteeners)
 
 
-#-- View the class attributes for the data frame for vector
+#-- View the class attributes for the data frame and vector
 class(fourteeners)
 class(fourteeners$Rank)
 
-#--calculate the average height elevation for fourteeners
-mean(fourteeners$Elevation)
+#--Count the records in a data frame
+nrow(fourteeners)
 
+#--Find which fourteeners I have climbed with 1 or more daughters
+fourteeners[fourteeners$With.Daughters > 0,]
+View(fourteeners[fourteeners$With.Daughters > 0,])
+
+#--Find which fourteeners I have not climbed with any daughters
+fourteeners[fourteeners$With.Daughters == 0,]
+
+#--calculate the average elevations for fourteeners
+mean(fourteeners$Elevation)
+WithDaughters=fourteeners[fourteeners$With.Daughters > 0,]
+mean(WithDaughters$Elevation)
 
 #--Create plot graphs of the elevation for fourteeners using lines, points, histograms, and both lines and points
 
@@ -35,9 +62,9 @@ plot(fourteeners$Member.Ascents, type="h")
 
 boxplot(fourteeners$Elevation)
 boxplot(fourteeners$Elevation, main = "Altitude in feet above Sea Level", xlab = "Elevation", ylab = "Rank", border = "brown", horizontal = TRUE, notch = TRUE)
+boxplot(WithDaughters$Elevation, main = "Altitude in feet above Sea Level", xlab = "Elevation", ylab = "Rank", border = "brown", horizontal = TRUE, notch = TRUE)
 
-
-#--Create a linear model and determine the P-value. There appears to be a to see there is a correlation between elevation and member ascents. 
+#--Create a linear model and determine the P-value. 
 
 linearmod <- lm ((fourteeners$Elevation) ~ (fourteeners$Member.Ascents), data = fourteeners)
 summary(linearmod)
